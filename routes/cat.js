e9df92a913e54dd1edae971ad30ac7d1408a420a
daichiice <dk7200a@gmail.com>
2026-07-06 21:02:20 +0900

    add cat api

--- routes/cat.js
+++ routes/cat.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-
 router.get('/', async (req, res) => {
-	request('https://api.thecatapi.com/v1/images/search', function (error, response,body) {
-		if (!error && response.statusCode == 200) {
-			const data = JSON.parse(body);
-			res.json(data);
-		}
-	});
+request('https://api.giphy.com/v1/gifs/random?api_key=mEgdW7JqkKuiTX3V7N7koAteLm5ef5yX', function (error, response, body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
+});
 })
-
 module.exports = router;
